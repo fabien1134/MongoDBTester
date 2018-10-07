8a8da167e5de2646680bf0ebae2ca127500ff89d
Fabien Bibana-Bessala <fabien1134@hotmail.co.uk>
2018-10-07 11:00:47 +0100

    Added CUD Option Menu Tool Strip Functionality

--- MongoDBTester/CollegeMemberManager.Designer.cs
+++ MongoDBTester/CollegeMemberManager.Designer.cs
@@ -28,18 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pnlFooter = new System.Windows.Forms.Panel();
-            this.pnlBody = new System.Windows.Forms.Panel();
+            this.btnInitialiseMongoDB = new System.Windows.Forms.Button();
+            this.btnDisplayCollegeMembers = new System.Windows.Forms.Button();
             this.ssStatusStrip = new System.Windows.Forms.StatusStrip();
             this.tspbExecutionState = new System.Windows.Forms.ToolStripProgressBar();
             this.tsslStatus = new System.Windows.Forms.ToolStripStatusLabel();
-            this.btnDisplayCollegeMembers = new System.Windows.Forms.Button();
-            this.btnInitialiseMongoDB = new System.Windows.Forms.Button();
+            this.pnlBody = new System.Windows.Forms.Panel();
             this.dgvResult = new System.Windows.Forms.DataGridView();
+            this.colMemberId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCollegeMemberName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCollegeMemberAge = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCollegeMemberType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cmsCrudOptions = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.updateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pnlFooter.SuspendLayout();
-            this.pnlBody.SuspendLayout();
             this.ssStatusStrip.SuspendLayout();
+            this.pnlBody.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).BeginInit();
+            this.cmsCrudOptions.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlFooter
@@ -53,14 +62,25 @@
             this.pnlFooter.Size = new System.Drawing.Size(800, 100);
             this.pnlFooter.TabIndex = 0;
             // 
-            // pnlBody
+            // btnInitialiseMongoDB
             // 
-            this.pnlBody.Controls.Add(this.dgvResult);
-            this.pnlBody.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlBody.Location = new System.Drawing.Point(0, 0);
-            this.pnlBody.Name = "pnlBody";
-            this.pnlBody.Size = new System.Drawing.Size(800, 350);
-            this.pnlBody.TabIndex = 1;
+            this.btnInitialiseMongoDB.Location = new System.Drawing.Point(370, 19);
+            this.btnInitialiseMongoDB.Name = "btnInitialiseMongoDB";
+            this.btnInitialiseMongoDB.Size = new System.Drawing.Size(136, 46);
+            this.btnInitialiseMongoDB.TabIndex = 2;
+            this.btnInitialiseMongoDB.Text = "Initialise MongoDB";
+            this.btnInitialiseMongoDB.UseVisualStyleBackColor = true;
+            this.btnInitialiseMongoDB.Click += new System.EventHandler(this.btnInitialiseMongoDB_Click);
+            // 
+            // btnDisplayCollegeMembers
+            // 
+            this.btnDisplayCollegeMembers.Location = new System.Drawing.Point(228, 19);
+            this.btnDisplayCollegeMembers.Name = "btnDisplayCollegeMembers";
+            this.btnDisplayCollegeMembers.Size = new System.Drawing.Size(136, 46);
+            this.btnDisplayCollegeMembers.TabIndex = 1;
+            this.btnDisplayCollegeMembers.Text = "Display College Members";
+            this.btnDisplayCollegeMembers.UseVisualStyleBackColor = true;
+            this.btnDisplayCollegeMembers.Click += new System.EventHandler(this.btnDisplayCollegeMembers_Click);
             // 
             // ssStatusStrip
             // 
@@ -82,37 +102,83 @@
             this.tsslStatus.Name = "tsslStatus";
             this.tsslStatus.Size = new System.Drawing.Size(0, 17);
             // 
-            // btnDisplayCollegeMembers
+            // pnlBody
             // 
-            this.btnDisplayCollegeMembers.Location = new System.Drawing.Point(228, 19);
-            this.btnDisplayCollegeMembers.Name = "btnDisplayCollegeMembers";
-            this.btnDisplayCollegeMembers.Size = new System.Drawing.Size(136, 46);
-            this.btnDisplayCollegeMembers.TabIndex = 1;
-            this.btnDisplayCollegeMembers.Text = "Display College Members";
-            this.btnDisplayCollegeMembers.UseVisualStyleBackColor = true;
-            this.btnDisplayCollegeMembers.Click += new System.EventHandler(this.btnDisplayCollegeMembers_Click);
-            // 
-            // btnInitialiseMongoDB
-            // 
-            this.btnInitialiseMongoDB.Location = new System.Drawing.Point(370, 19);
-            this.btnInitialiseMongoDB.Name = "btnInitialiseMongoDB";
-            this.btnInitialiseMongoDB.Size = new System.Drawing.Size(136, 46);
-            this.btnInitialiseMongoDB.TabIndex = 2;
-            this.btnInitialiseMongoDB.Text = "Initialise MongoDB";
-            this.btnInitialiseMongoDB.UseVisualStyleBackColor = true;
-            this.btnInitialiseMongoDB.Click += new System.EventHandler(this.btnInitialiseMongoDB_Click);
+            this.pnlBody.Controls.Add(this.dgvResult);
+            this.pnlBody.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlBody.Location = new System.Drawing.Point(0, 0);
+            this.pnlBody.Name = "pnlBody";
+            this.pnlBody.Size = new System.Drawing.Size(800, 350);
+            this.pnlBody.TabIndex = 1;
             // 
             // dgvResult
             // 
             this.dgvResult.AllowUserToAddRows = false;
             this.dgvResult.AllowUserToDeleteRows = false;
             this.dgvResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResult.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colMemberId,
+            this.colCollegeMemberName,
+            this.colCollegeMemberAge,
+            this.colCollegeMemberType});
+            this.dgvResult.ContextMenuStrip = this.cmsCrudOptions;
             this.dgvResult.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvResult.Location = new System.Drawing.Point(0, 0);
             this.dgvResult.Name = "dgvResult";
             this.dgvResult.ReadOnly = true;
+            this.dgvResult.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvResult.Size = new System.Drawing.Size(800, 350);
             this.dgvResult.TabIndex = 0;
+            // 
+            // colMemberId
+            // 
+            this.colMemberId.HeaderText = "MemberId";
+            this.colMemberId.Name = "colMemberId";
+            this.colMemberId.ReadOnly = true;
+            this.colMemberId.Visible = false;
+            // 
+            // colCollegeMemberName
+            // 
+            this.colCollegeMemberName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colCollegeMemberName.HeaderText = "College Member Name";
+            this.colCollegeMemberName.Name = "colCollegeMemberName";
+            this.colCollegeMemberName.ReadOnly = true;
+            // 
+            // colCollegeMemberAge
+            // 
+            this.colCollegeMemberAge.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colCollegeMemberAge.HeaderText = "College Member Age";
+            this.colCollegeMemberAge.Name = "colCollegeMemberAge";
+            this.colCollegeMemberAge.ReadOnly = true;
+            // 
+            // colCollegeMemberType
+            // 
+            this.colCollegeMemberType.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colCollegeMemberType.HeaderText = "College Member Type";
+            this.colCollegeMemberType.Name = "colCollegeMemberType";
+            this.colCollegeMemberType.ReadOnly = true;
+            // 
+            // cmsCrudOptions
+            // 
+            this.cmsCrudOptions.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.updateToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.cmsCrudOptions.Name = "cmsCrudOptions";
+            this.cmsCrudOptions.Size = new System.Drawing.Size(113, 48);
+            this.cmsCrudOptions.Opening += new System.ComponentModel.CancelEventHandler(this.cmsCrudOptions_Opening);
+            this.cmsCrudOptions.Paint += new System.Windows.Forms.PaintEventHandler(this.cmsCrudOptions_Paint);
+            // 
+            // updateToolStripMenuItem
+            // 
+            this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.updateToolStripMenuItem.Text = "Update";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
             // 
             // frmCollegeMemberManager
             // 
@@ -126,10 +192,11 @@
             this.Load += new System.EventHandler(this.frmCollegeMemberManager_Load);
             this.pnlFooter.ResumeLayout(false);
             this.pnlFooter.PerformLayout();
-            this.pnlBody.ResumeLayout(false);
             this.ssStatusStrip.ResumeLayout(false);
             this.ssStatusStrip.PerformLayout();
+            this.pnlBody.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).EndInit();
+            this.cmsCrudOptions.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -144,6 +211,13 @@
         private System.Windows.Forms.ToolStripStatusLabel tsslStatus;
         private System.Windows.Forms.Panel pnlBody;
         private System.Windows.Forms.DataGridView dgvResult;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMemberId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCollegeMemberName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCollegeMemberAge;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCollegeMemberType;
+        private System.Windows.Forms.ContextMenuStrip cmsCrudOptions;
+        private System.Windows.Forms.ToolStripMenuItem updateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
     }
 }
 
--- MongoDBTester/CollegeMemberManager.cs
+++ MongoDBTester/CollegeMemberManager.cs
@@ -10,25 +10,52 @@ namespace MongoDBTester
 {
     public partial class frmCollegeMemberManager : Form
     {
+        //Will map the college member to their grades
+        private Dictionary<string, Studentqualification[]> m_collegeMemberStudentQualificationMapping = default;
+        //Will temporarily store removed context menu items
+        private List<(int OriginalPosition, ToolStripItem MenuItem)> m_removedToolstripItems = new List<(int OriginalPosition, ToolStripItem MenuItem)>();
+
         public frmCollegeMemberManager() => InitializeComponent();
 
+        //Initialise DB when the form loads
         private void frmCollegeMemberManager_Load(object sender, EventArgs e) => InitialiseDB();
 
         private void btnDisplayCollegeMembers_Click(object sender, EventArgs e)
         {
-            var collegeMembers = MongoDbConnector.MongoDatabase.GetCollection<CollegeMember>("CollegeMember");
-            var collegeMembersCollection = collegeMembers.AsQueryable().ToList();
+            try
+            {   //Ensure previous items are removed
+                m_collegeMemberStudentQualificationMapping = new Dictionary<string, Studentqualification[]>();
+                dgvResult.Rows.Clear();
+                Dictionary<string, string> collegeMemberTypeNameGuidMapping = new Dictionary<string, string>();
+             
+                //Ensure all stuff GUID's are replaced with 
+                MongoDbConnector.MongoDatabase.GetCollection<CollegeMemberType>("CollegeMemberType").AsQueryable().ToList().ForEach((cmt) =>
+                {
+                    collegeMemberTypeNameGuidMapping.Add(cmt.Id.ToString(), cmt.Type);
+                });
 
-            var collegeMemberTypeNameGuidMapping = new Dictionary<string, string>();
-            //Ensure all stuff GUID's are replaced with 
-            MongoDbConnector.MongoDatabase.GetCollection<CollegeMemberType>("CollegeMemberType").AsQueryable().ToList().ForEach((cmt) =>
-            {
-                collegeMemberTypeNameGuidMapping.Add(cmt.Id.ToString(), cmt.Type);
-            });
+                //There must be college member types defined
+                if (collegeMemberTypeNameGuidMapping.Count() == 0)
+                    throw new Exception("Ensure College Member Types Are Present");
 
-            foreach (CollegeMember collegeMember in collegeMembersCollection)
+                //Get all college members
+                List<CollegeMember> collegeMembersCollection = MongoDbConnector.MongoDatabase.GetCollection<CollegeMember>("CollegeMember").AsQueryable().ToList(); ;
+                
+                //Iterate through college members
+                collegeMembersCollection.ForEach((collegeMember) =>
+                {   //Try and retrieve the college member type
+                    collegeMemberTypeNameGuidMapping.TryGetValue(collegeMember.Type, out string type);
+
+                    //Display college member type in the display
+                    string collegeMemberId = collegeMember.Id.ToString();
+                    dgvResult.Rows.Add(new string[] { collegeMemberId, collegeMember.Name, collegeMember.Age.ToString(), type });
+                    //Add a college member and qualification mapping
+                    m_collegeMemberStudentQualificationMapping.Add(collegeMemberId, collegeMember.StudentQualification);
+                });
+            }
+            catch (Exception ex)
             {
-                string type = collegeMemberTypeNameGuidMapping[collegeMember.Type];
+                MessageBox.Show(this, ex.Message, "Issue Loading College Members");
             }
         }
 
@@ -47,6 +74,60 @@ namespace MongoDBTester
             {
                 MessageBox.Show(this, ex.Message, "Issue Initialising Database");
                 tsslStatus.Text = "Database Connection Failure";
+            }
+        }
+
+        //Will close the 
+        private void cmsCrudOptions_Paint(object sender, PaintEventArgs e)
+        {
+            if (dgvResult.SelectedRows.Count == 0)
+                cmsCrudOptions.Close();
+        }
+
+        private void cmsCrudOptions_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (dgvResult.SelectedRows.Count != 0)
+            {
+                //Ensure the user will not be able to delete items when 
+                if (dgvResult.SelectedRows.Count >= 2)
+                {
+                    if (cmsCrudOptions.Items.Count > 0)
+                        RemoveMenutoolstripItem("Update");
+                }
+                else if (dgvResult.SelectedRows.Count == 1)
+                {
+                    if (m_removedToolstripItems.Count > 0)
+                        ReplaceRemovedToolStripItems();
+                }
+            }
+        }
+
+        //Re-Insert all removed toolstrip menu items
+        private void ReplaceRemovedToolStripItems()
+        {
+            for (int i = 0; i < m_removedToolstripItems.Count; i++)
+            {   //Re-insert menu item in its original position
+                (int OriginalPosition, ToolStripItem MenuItem) removedToolStripItemDetails = m_removedToolstripItems[i];
+                cmsCrudOptions.Items.Insert(removedToolStripItemDetails.OriginalPosition, removedToolStripItemDetails.MenuItem);
+            }
+            //Remove un-required stored items
+            m_removedToolstripItems.Clear();
+        }
+
+
+        //Will remove the desired menu item is more then one college member is selected
+        private void RemoveMenutoolstripItem(string toolstripMenuItemText)
+        {
+            foreach (ToolStripItem item in cmsCrudOptions.Items)
+            {
+                if (string.Compare(item.Text, toolstripMenuItemText) == 0)
+                {
+                    //Store details of the removed menu item
+                    int indexOfRemovedToolStripItem = cmsCrudOptions.Items.IndexOf(item);
+                    m_removedToolstripItems.Add((indexOfRemovedToolStripItem, item));
+                    cmsCrudOptions.Items.Remove(item);
+                    break;
+                }
             }
         }
     }
